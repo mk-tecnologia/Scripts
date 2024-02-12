ca19e602c0e0ed83111ff911dc75721148bb83d1
Marcos A <marcos@mktecnologia.net.br>
2024-02-12 13:14:16 -0300

    Update initDeb.sh

--- initDeb.sh
+++ initDeb.sh
@@ -4,10 +4,8 @@
 # License: MIT
 # initDeb.sh faz as configurações iniciais do GNU/DEBIAN
 
-#USUARIO ATUAL
+#VARIAVEIS
 usuario=$USER
-
-#DEFINE ESPERAS
 esperar="sleep 3"
 reiniciar="sleep 5"
 
@@ -23,53 +21,91 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+func_message() {
+local func_message="${purple}$message${nc}"
+local message_length=${#func_message}
+local ok_position=$((80 - message_length))
+local dots_count=$((ok_position - 5))
+echo -ne "$func_message"
+for ((i = 0; i < dots_count; i++)); do
+    echo -n "."
+done
+echo -e "${green}[OK]${nc}"
+}
+
 # MENSAGEM INICIAL
 clear
+echo "##############################"
+echo "## SCRIPT CONFIG GNU/DEBIAN ##"
+echo "##############################"
+echo ""
+echo -e "${purple}## INICIANDO CONFIG DO GNU/DEBIAN ##${nc}"
+echo ""
+
+
 echo -ne "${purple}INICIANDO CONFIGURAÇÕES DO SISTEMA...${nc}"
 echo ""
 $esperar
 
 # INSTALAÇÃO DE PACOTES
-echo -ne "${purple}INSTALANDO PACOTES...${nc}"
-apt-get update > /dev/null 2>&1
-apt-get install -y vim neofetch qemu-guest-agent > /dev/null 2>&1
-echo -e "${green}OK${nc}"
-$esperar
+func_install_pkg() {
+   local message="INSTALANDO PACOTES"
+  apt-get update > /dev/null 2>&1
+  apt-get install -y vim neofetch qemu-guest-agent > /dev/null 2>&1
+  func_message
+  $esperar
+}
 
 # DESATIVA SSH PERMITROOTLOGIN
-echo -ne "${purple}DESATIVANDO PERMITROOTLOGIN NO SSH...${nc}"
-sed -i 's/^PermitRootLogin .*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
-service ssh restart > /dev/null
-echo -e "${green}OK${nc}"
-$esperar
+func_disable_ssh() {
+   local message="DESATIVANDO PERMITROOTLOGIN NO SSH"
+  sed -i 's/^PermitRootLogin .*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
+  service ssh restart > /dev/null
+  func_message
+  $esperar
+}
 
-# COPIAR CHAVE SSH
-echo -ne "${purple}COPIANDO CHAVE SSH..${nc}"
-cat << EOF | tee /${usuario}/.ssh/authorized_keys > /dev/null
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDW2K3YdN9/O18AVtiyOt5b5rBHUDxu3iuUghV/I2vli3ZxNL0HxmlDjMvb4EsP4Y5WD9J16rDCWGA7QGEfj5ZhKuJRkyZVKJDCr8E99NhLTd8CvxYDBQY88kKyy2e47asSpAZGZwW74OgxbkYC6yeG1nz4Gxi2sTkfa27yXea3gewuT5ozvWtskjkyNc47WrMXJWQ6khMqe+doQQkZXUVrZZp3lp1lGRy8I5FYBAXmRntWTMF7RfLURh6bNA2PRc5mLLaePq8cwLHfz2ucnmHXK/EA4vFCAAchKs3OlEZZ/y72PPZAz/A7OP3jo+kgJFOe+qaTjHZCU1XsPMU3ezu3 Caverna@notecaverna.local
-EOF
-echo -e "${green}OK${nc}"
+func_add_ssh_key() {
+  local chave_publica="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDW2K3YdN9/O18AVtiyOt5b5rBHUDxu3iuUghV/I2vli3ZxNL0HxmlDjMvb4EsP4Y5WD9J16rDCWGA7QGEfj5ZhKuJRkyZVKJDCr8E99NhLTd8CvxYDBQY88kKyy2e47asSpAZGZwW74OgxbkYC6yeG1nz4Gxi2sTkfa27yXea3gewuT5ozvWtskjkyNc47WrMXJWQ6khMqe+doQQkZXUVrZZp3lp1lGRy8I5FYBAXmRntWTMF7RfLURh6bNA2PRc5mLLaePq8cwLHfz2ucnmHXK/EA4vFCAAchKs3OlEZZ/y72PPZAz/A7OP3jo+kgJFOe+qaTjHZCU1XsPMU3ezu3 Caverna@notecaverna.local"
+  local arquivo_chaves="/${usuario}/.ssh/authorized_keys"
+
+  # VERIFICA SE A CHAVE PÚBLICA JÁ ESTÁ PRESENTE NO ARQUIVO
+  if grep -qF "$chave_publica" "$arquivo_chaves"; then
+    local message="CHAVE SSH JÁ CONFIGURADA"
+    func_message
+  else
+    # ADICIONA A CHAVE PÚBLICA AO ARQUIVO
+    local message="ADICIONANDO CHAVE SSH"
+    echo "$chave_publica" >> "$arquivo_chaves"
+    func_message
+  fi
+}
 
 # AJUSTA NEOFETCH
-echo -ne "${purple}CONFIGURANDO INFORMAÇÕES DO SISTEMA NO LOGIN...${nc}"
-cat << 'EOF' | tee /etc/update-motd.d/10-uname > /dev/null
+func_set_neofetch() {
+  local message="CONFIGURANDO INFORMAÇÕES DO SISTEMA NO LOGIN"
+  cat << 'EOF' | tee /etc/update-motd.d/10-uname > /dev/null
 #!/bin/sh
 echo ""
 neofetch
 EOF
 chmod +x /etc/update-motd.d/10-uname
-echo -e "${green}OK${nc}"
+func_message
 $esperar
+}
 
 # AJUSTA MOTD
-echo -ne "${purple}LIMPANDO MOTD...${nc}"
-echo "" > /etc/motd
-echo -e "${green}OK${nc}"
-$esperar
+func_set_motd() {
+  local message="LIMPANDO MOTD"
+  echo "" > /etc/motd
+  func_message
+  $esperar
+}
 
 # AJUSTA ISSUE
-echo -ne "${purple}ATUALIZANDO ARQUIVO ISSUE...${nc}"
-cat << EOF | tee /etc/issue > /dev/null
+func_set_issue() {
+  local message="ATUALIZANDO ARQUIVO ISSUE"
+  cat << EOF | tee /etc/issue > /dev/null
 
  ███╗   ███╗██╗  ██╗████████╗███████╗ ██████╗███╗   ██╗ ██████╗ ██╗      ██████╗  ██████╗ ██╗ █████╗
  ████╗ ████║██║ ██╔╝╚══██╔══╝██╔════╝██╔════╝████╗  ██║██╔═══██╗██║     ██╔═══██╗██╔════╝ ██║██╔══██╗
@@ -93,38 +129,54 @@ cat << EOF | tee /etc/issue > /dev/null
 
 EOF
 chattr +i /etc/issue > /dev/null
-echo -e "${green}OK${nc}"
+func_message
 $esperar
+}
 
 # AJUSTA .BASHRC
-echo -ne "${purple}CONFIGURANDO .BASHRC...${nc}"
-cat << 'EOF' >> /${usuario}/.bashrc
-# Adicionado pelo script de configuração
-export LS_OPTIONS='--color=auto'
-eval "`dircolors`"
-alias ls='ls $LS_OPTIONS'
-alias ll='ls $LS_OPTIONS -l'
-alias l='ls $LS_OPTIONS -lA'
-alias rm='rm -i'
-alias cp='cp -i'
-alias mv='mv -i'
-EOF
-echo -e "${green}OK${nc}"
+func_set_basrc() {
+  local message="CONFIGURANDO .BASHRC"
+  file="/${usuario}/.bashrc"
+  sed -i 's/^# export LS_OPTIONS='\''--color=auto'\''$/export LS_OPTIONS='\''--color=auto'\''/' "$file"
+  sed -i 's/^# eval "\$(dircolors)"$/eval "\$(dircolors)"/' "$file"
+  sed -i 's/^# alias ls='\''ls \$LS_OPTIONS'\''$/alias ls='\''ls \$LS_OPTIONS'\''/' "$file"
+  sed -i 's/^# alias ll='\''ls \$LS_OPTIONS -l'\''$/alias ll='\''ls \$LS_OPTIONS -l'\''/' "$file"
+  sed -i 's/^# alias l='\''ls \$LS_OPTIONS -lA'\''$/alias l='\''ls \$LS_OPTIONS -lA'\''/' "$file"
+  sed -i 's/^# \(alias rm='\''rm -i'\''\)/\1/' "$file"
+  sed -i 's/^# \(alias cp='\''cp -i'\''\)/\1/' "$file"
+  sed -i 's/^# \(alias mv='\''mv -i'\''\)/\1/' "$file"
+func_message
 $esperar
+}
 
 # SOLICITAR NOME DE DOMÍNIO AO USUÁRIO
-echo -ne "${purple}DIGITE O NOME DO DOMÍNIO${nc} ${green}(EXEMPLO: MINHAEMPRESA.INTRANET):${nc} "
-read dominio
+func_get_domain() {
+  echo -ne "${purple}DIGITE O NOME DO DOMÍNIO${nc} ${green}(EXEMPLO: MINHAEMPRESA.INTRANET):${nc} "
+  read dominio
+}
 
 # AJUSTA NOME DE DOMÍNIO
-echo -ne "${purple}CONFIGURANDO NOME DE DOMÍNIO PARA${nc} ${green}$dominio...${nc}"
-echo "kernel.domainname = $dominio" >> /etc/sysctl.conf
-sysctl -p > /dev/null
-echo -e "${green}OK${nc}"
-$esperar
+func_set_domain() {
+  local message="CONFIGURANDO NOME DE DOMÍNIO PARA: ${green}$dominio${nc}  "
+  sed -i "/#kernel.domainname = example.com/akernel.domainname = $domainname" /etc/sysctl.conf
+  sysctl -p > /dev/null
+  func_message
+  $esperar
+}
+
+# EXECUTA FUNÇÕES
+func_install_pkg
+func_disable_ssh
+func_add_ssh_key
+func_set_neofetch
+func_set_motd
+func_set_issue
+func_set_basrc
+func_get_domain
+func_set_domain
 
 # MENSAGEM DE CONCLUSÃO
 echo -ne "${green}CONFIGURAÇÕES CONCLUÍDAS...${nc}"
-echo "REINICIANDO"
+echo -e "${red}REINICIANDO${nc}"
 $reiniciar
 init 6
